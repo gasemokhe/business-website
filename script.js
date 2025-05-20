938299b49e9dead087a96695a086fab1ee9e8c29
gasemokhe <73920488+gasemokhe@users.noreply.github.com>
2025-05-20 12:25:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent.");
+  });
+  
+  document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+});
+
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
